8bf08d38a5a16113538898fe84adb5f76d829074
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2020-06-04 18:24:45 -0700

    added settings for reach out script

---
+++ settings.py
@@ -0,0 +1,14 @@
+#############################
+#  reach out script config  #
+#############################
+
+settings = {
+
+	"name": "John Doe",
+	"username": "johndoe",
+	"number": "408-123-456",
+	"hours": "9am-6pm",
+	"tmz": "PST",
+
+	"script_location": "Desktop",
+}
