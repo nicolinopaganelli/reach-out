542ed63c6aaedced1eb4fb46a8b2050d75af3990
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2020-06-08 20:07:57 -0700

    Added description for script_location

--- settings.py
+++ settings.py
@@ -9,6 +9,10 @@ settings = {
 	"number": "408-123-456",
 	"hours": "9am-6pm",
 	"tmz": "PST",
-
+# Location prefix = C:\Users\[username]\"script_location"
+# You can change this by editing the PATH variable in "reach out.py"
 	"script_location": "Desktop",
+
+# Would allow GUI to be optional, but it is forced for now.
+#	"gui":		    True,
 }
